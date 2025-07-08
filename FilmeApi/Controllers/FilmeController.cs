9434ca05cd74166aa99eed4b5393dca21a287e7d
Manoela Urias <maanoelaurias@gmail.com>
2025-07-07 21:41:45 -0300

    Adicionar arquivos de projeto.

---
+++ FilmeApi/Controllers/FilmeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController : ControllerBase
+{
+    private static Lista<Filme> filmes = new Lista<Filme>();
+
+    public void AdicionaFilme(Filme filme)
+    {
+        filmes.Add(filme);
+    }
+}
